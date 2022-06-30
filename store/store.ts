ee95631494fe24cbd1e7fe87d001cf8118655168
galifornia <martinho87@gmail.com>
2022-06-30 18:37:12 +0200

    Add Zustand dep

---
+++ store/store.ts
@@ -0,0 +1,41 @@
+import create from 'zustand';
+import { devtools, persist } from 'zustand/middleware';
+
+interface AppState {
+  // user
+  loggedIn: boolean;
+  setLoggedIn: (newState: boolean) => void;
+  // available for user polls
+  polls: Poll[];
+  fetchPolls: () => void;
+  // votes
+  selectedVotes: Vote[];
+  fetchVotes: (pollId: string) => void;
+}
+
+type Vote = {
+  pollId: string;
+};
+
+type Poll = {};
+
+export const useStore = create<AppState>()(
+  devtools(
+    persist((set) => ({
+      loggedIn: false,
+      setLoggedIn: (isLogged) => set(() => ({ loggedIn: isLogged })),
+      polls: [],
+      fetchPolls: async () => {
+        const response = await fetch('http://localhost:3000/polls');
+        const polls: Poll[] = await response.json();
+        set({ polls: polls });
+      },
+      selectedVotes: [],
+      fetchVotes: async (pollId: string) => {
+        const response = await fetch('http://localhost:3000/votes');
+        const votes: Vote[] = await response.json();
+        set({ selectedVotes: votes.filter((vote) => vote.pollId == pollId) });
+      },
+    }))
+  )
+);
